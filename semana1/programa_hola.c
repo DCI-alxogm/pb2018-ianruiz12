1664292d38d269957b3a7de0275b8558eba83e5f
Ruiz Bueno Ian Daniel <ruizbi2017@localhost.localdomain>
2018-08-06 10:04:14 -0500

    Añadi archivo

---
+++ semana1/programa_hola.c
@@ -0,0 +1,7 @@
+/*
+     Esto es un comentario. Me sirve para escribir información relevante del pro
+grama que estoy haciendo. Por  ejemplo:
+     Este programa imprime la típica frase “hola mundo a la pantalla”
+     Creado el 06/08/2018 por Ian Daniel Ruiz Bueno
+    */
+
