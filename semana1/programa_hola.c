a21c82cd52e20840bfc3ef705c9ffb9c16e51494
Ruiz Bueno Ian Daniel <ruizbi2017@localhost.localdomain>
2018-08-08 09:25:43 -0500

    Cree mi primer programa.

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,7 +1,22 @@
 /*
      Esto es un comentario. Me sirve para escribir información relevante del pro
 grama que estoy haciendo. Por  ejemplo:
-     Este programa imprime la típica frase “hola mundo a la pantalla”
+     Este programa imprime la típica frase “bienvenido al mundo de la programacion”
      Creado el 06/08/2018 por Ian Daniel Ruiz Bueno
     */
+
+/* Creado el 8 de agosto del 2018 por Ian Daniel Ruiz Bueno */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hola Ian \n");
+	printf("Este es el primer programa del curso\n");
+
+}
+
+/*Termine de escribir el primer programa*/
+
+
 
