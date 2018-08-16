bcc7a3684d322d9c736825a7cf945978f6bbe35e
ianruiz12 <idrb1212@hotmail.com>
2018-08-16 15:33:39 -0500

    este es el tercer ejercicio

---
+++ semana2/ejercicio_3.c
@@ -0,0 +1,26 @@
+/*este es el ejercicio numero 3 con numeros reales*/
+
+#include<stdio.h>
+
+int main(){
+
+          float a,b,c,d,e;
+          printf("introduce el primer numero: \n");
+          scanf("%f",&a);	  
+          printf("introduce el segundo numero: \n");
+	  scanf("%f",&b);
+	  printf("introduce el tercer numero: \n");
+	  scanf("%f",&c);
+	  printf("introduce el cuarto numero: \n");
+	  scanf("%f",&d);
+ 	  e=(a+b)*c/d;
+	  printf("%f \n",e);
+          e=((a+b)*c)/d;
+          printf("%f \n",e);
+	  e=(a+b)*c/d;
+	  printf("%f \n",e);
+	  e=a+(b*c)/d;
+          printf("%f \n",e);
+
+    
+}
