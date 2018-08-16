077ae722a1d630726a0de75b9e6bf8bfc416430a
ianruiz12 <idrb1212@hotmail.com>
2018-08-16 15:25:48 -0500

    este es el ejercicio 2

---
+++ semana2/ejercicio_2.c
@@ -0,0 +1,26 @@
+/*este es el ejercicio numero 2 con numeros enteros*/
+
+#include<stdio.h>
+
+int main(){
+
+          int a,b,c,d,e;
+          printf("introduce el primer numero: \n");
+          scanf("%i",&a);	  
+          printf("introduce el segundo numero: \n");
+	  scanf("%i",&b);
+	  printf("introduce el tercer numero: \n");
+	  scanf("%i",&c);
+	  printf("introduce el cuarto numero: \n");
+	  scanf("%i",&d);
+ 	  e=(a+b)*c/d;
+	  printf("%i \n",e);
+          e=((a+b)*c)/d;
+          printf("%i \n",e);
+	  e=(a+b)*c/d;
+	  printf("%i \n",e);
+	  e=a+(b*c)/d;
+   printf("%i \n",e);
+
+    
+}
