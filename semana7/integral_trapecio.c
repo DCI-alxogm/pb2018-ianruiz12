5da5df613e85cc128bfbc37cff43ba2d7281ce8b
Ruiz Bueno Ian Daniel <ruizbi2017@localhost.localdomain>
2018-09-21 17:02:37 -0500

    finish

--- semana7/integral_trapecio.c
+++ semana7/integral_trapecio.c
@@ -6,18 +6,49 @@
 
 int main(){
            FILE *fp;
-           float i, a, b, c, N, p, h, k, n,fa, fx, I; 
-           fp=fopen("datos.txt","r+w")
-           fprintf("Ingrese la potencia: \n");
-           fscanf("%f", &p);
-           fprintf("Ingrese el valor minimo del intervalo: \n");
-           fscanf("%f", &a);
-           fprintf("ingrese el valor maximo del intervalo: \n");
-           fscanf("%f", &b);
-           fprintf("ingrese el numero de veces que se evaluara: \n");
-           fscanf("%f", &N);
-           c=(b-a)/N;
-           if(p>=1){
-                   fa=pow(a,p);
+           int j, i, p, N, n;
+           float a, b, c, fa, h, suma; 
+           fp=fopen("datos.txt","r");
+           fscanf(fp, "%i, %f, %f, %i, %i", &p, &a, &b, &N, &n);
+           printf("%i, %f, %f, %i, %i \n", p, a, b, N, n);
+           fclose(fp);
+           float x[N], fx[N], intx[N];     
+           if(p<0){
+                  printf("La potencia es negativa \n");
+                  return 1;
+                  }      
+           for(i=0;i<N;i++){
+                           x[i]=0;
+                           fx[i]=0;
+                           intx[i]=0;
+                           }
+           for(i=0;i<N;i++){
+                           x[i]=0;
+                           fx[i]=0;
+                           intx[i]=0;
+                           }
+          c=(b-a)/N;
+          for(i=0;i<N;i++){
+                          x[i]=a+(i*c);
+                          fx[i]=pow(x[i],p);
+                          }
+          fa=pow(a,p);
+          for(i=0;i<N;i++){
+                          h=(x[i]-a)/n;
+                          suma=0;
+                          for(j=1;j<n;j++){
+                                           suma=suma+(pow((x[i]+(h*j)),p));
+                                          }
+                          intx[i]=((fx[i]+fa)/2.0);
+                          }
+          fp=fopen("resultados.txt", "w"); 
+          fprintf(fp, "x , fx , int(x) \n");
+          for(i=0;i<N;i++){
+                          fprintf(fp," %f  %f  %f", x[i], fx[i], intx[i]);
+                          }
+          fclose(fp);
+          return 0;
+          }
+                  
          
            
